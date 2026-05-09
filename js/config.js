2c4caa00dbcf89e922b3f7036be3359703dcadde
brownieSea <165341873+brownieSea@users.noreply.github.com>
2026-05-09 19:38:22 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,25 +3,12 @@
 // ============================================================
 
 const CONFIG = {
-
   PASSWORD: "9988",
-
   SHEET_CSV_URL: "https://docs.google.com/spreadsheets/d/1gWwvknhYm7VBbzRNl2dA561QD5NdlIiB3KkRTcxmau0/gviz/tq?tqx=out:csv",
-
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/1gWwvknhYm7VBbzRNl2dA561QD5NdlIiB3KkRTcxmau0/exec",
-
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzvbQJDKYYAlW0vEbOhlhpFx5orWnjNnSqRO_B3wkyshbw411xYTY_f9u9egRG_enYaYA/exec",
   COLUMNS: {
-    date:         0,
-    time:         1,
-    glucose:      2,
-    lastMealTime: 3,
-    meal:         4,
-    oilYN:        5,
-    meta:         6,
-    lemon:        7,
-    note:         8,
-    sleep:        9,
-    prevMed:     10,
-    lastOil:     11,
+    date: 0, time: 1, glucose: 2, lastMealTime: 3,
+    meal: 4, oilYN: 5, meta: 6, lemon: 7,
+    note: 8, sleep: 9, prevMed: 10, lastOil: 11,
   },
 };
